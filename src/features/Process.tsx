2758d0dffad06eaf97abb621d59f5148fcb0362f
Kharl <enriquezkharl14@gmail.com>
2024-06-20 09:16:16 +0800

    [Feat] Process - Client experience

--- src/features/Process.tsx
+++ src/features/Process.tsx
@@ -1,4 +1,6 @@
+import { Card, CardBody } from '@nextui-org/card';
 import { Divider } from '@nextui-org/divider';
+import { Avatar } from '@nextui-org/react';
 import { FaStarOfLife } from 'react-icons/fa6';
 
 export default function Process() {
@@ -7,7 +9,7 @@ export default function Process() {
       <div className="mx-4 pt-16 mb-8">
         <h1 className="text-2xl font-semibold mb-8">Our Process</h1>
 
-        <div>
+        <div className="mb-8">
           <Divider />
           <div className="my-4 flex flex-col space-y-8">
             <div className="flex items-center space-x-2">
@@ -57,6 +59,29 @@ export default function Process() {
             </p>
           </div>
           <Divider />
+        </div>
+
+        <div>
+          <h2 className="font-semibold  text-xl mb-8 flex items-center space-x-2">
+            <FaStarOfLife size={12} />
+
+            <span>Client Experience</span>
+          </h2>
+          <Card className="bg-[#ebe3d8]">
+            <CardBody>
+              <div className="flex items-center space-x-4 mb-4">
+                <Avatar src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                <div className="flex  flex-col text-sm">
+                  <span className="font-semibold">David Kahn</span>
+                  <span className="text-xs text-gray-500">Chief Executive Officer</span>
+                </div>
+              </div>
+              <p className="font-serif">
+                &ldquo;Working with Stem has been transformative experience. They occupy the
+                impossible space between medical research and responsible capitalism.&rdquo;
+              </p>
+            </CardBody>
+          </Card>
         </div>
       </div>
     </div>
