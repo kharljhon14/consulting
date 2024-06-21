86ab3d788eb06a5611b771e195896a0d7342b856
Kharl <enriquezkharl14@gmail.com>
2024-06-21 23:49:12 +0800

    [Style] News desktop design

--- src/features/News.tsx
+++ src/features/News.tsx
@@ -7,24 +7,26 @@ export default function News() {
   return (
     <div className="bg-secondary">
       <div className="text-primary mx-4 pt-16 pb-8">
-        <h1 className="text-2xl font-semibold">Latest News</h1>
+        <h1 className="text-2xl font-semibold lg:text-4xl">Latest News</h1>
         <Divider className="my-8" />
         <div className=" flex flex-col space-y-10">
           <Link href="/">
-            <div>
-              <Image
-                alt=""
-                src="https://images.unsplash.com/photo-1582719366794-4d27cd0a34a0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
-              <div className="mt-6 space-y-4">
-                <div className="flex justify-between">
+            <div className="lg:flex lg:space-x-6 lg:py-6">
+              <div className="lg:flex-1">
+                <Image
+                  alt=""
+                  src="https://images.unsplash.com/photo-1582719366794-4d27cd0a34a0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+              </div>
+              <div className="mt-6 lg:flex-1 lg:mt-0 lg:flex lg:flex-col">
+                <div className="flex justify-between mb-6 lg:justify-start lg:space-x-6">
                   <div className="flex items-center space-x-2">
                     <FaStarOfLife size={12} />
                     <span>News</span>
                   </div>
                   <time className="underline">September 26, 2024</time>
                 </div>
-                <h1 className="text-xl">
+                <h1 className="text-xl lg:text-3xl lg:mt-auto">
                   Discovering the next frontier in biomedical research through collaborarion
                 </h1>
               </div>
@@ -32,20 +34,22 @@ export default function News() {
           </Link>
           <Divider />
           <Link href="/">
-            <div>
-              <Image
-                alt=""
-                src="https://plus.unsplash.com/premium_photo-1676325102583-0839e57d7a1f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
-              <div className="mt-6 space-y-4">
-                <div className="flex justify-between">
+            <div className="lg:flex lg:space-x-6 lg:py-6">
+              <div className="lg:flex-1">
+                <Image
+                  alt=""
+                  src="https://plus.unsplash.com/premium_photo-1676325102583-0839e57d7a1f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+              </div>
+              <div className="mt-6 lg:flex-1 lg:mt-0 lg:flex lg:flex-col">
+                <div className="flex justify-between lg:justify-start lg:space-x-6">
                   <div className="flex items-center space-x-2">
                     <FaStarOfLife size={12} />
                     <span>Insight</span>
                   </div>
                   <time className="underline">April 14, 2024</time>
                 </div>
-                <h1 className="text-xl">
+                <h1 className="text-xl lg:text-3xl lg:mt-auto">
                   The role of community in accelerating scientific breakthroughs
                 </h1>
               </div>
