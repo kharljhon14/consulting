52ce4361317ac65964ae2cf7c5e3158e570ab44e
Kharl <enriquezkharl14@gmail.com>
2024-06-21 11:07:14 +0800

    [Feat] Hero desktop

--- src/features/Hero.tsx
+++ src/features/Hero.tsx
@@ -1,35 +1,39 @@
 import { Button } from '@nextui-org/button';
 import { Image } from '@nextui-org/image';
-import { Divider, Spacer } from '@nextui-org/react';
+import { Divider } from '@nextui-org/react';
 
 export default function Hero() {
   return (
-    <div className="mt-6 text-secondary mx-4">
-      <div className=" h-72">
-        <h1 className="text-6xl">Uniting Research with Capital</h1>
+    <div className="mt-10 text-secondary mx-4">
+      <div className="h-72">
+        <h1 className="text-6xl lg:text-8xl">
+          Uniting Research <span className="lg:block">with Capital</span>
+        </h1>
       </div>
 
       <Divider className="my-6 bg-secondary" />
-      <div className="pb-8">
-        <div className="my-8 flex justify-center flex-col">
-          <p className="leading-6 text-center">
-            Stem expedites the journey of groundbreaking healthcare solutions from conception to
-            global markets.
-          </p>
-          <Button className="mt-8 bg-primary text-secondary">Buy Stem</Button>
+      <div className="pb-8 lg:flex items-center justify-between">
+        <div className="my-8 flex justify-center lg:flex-1 lg:items-start flex-col">
+          <div className="text-center lg:text-start lg:text-lg lg:max-w-sm">
+            <p className="leading-6 ">
+              Stem expedites the journey of groundbreaking healthcare solutions from conception to
+              global markets.
+            </p>
+          </div>
+          <Button className="mt-8 bg-primary text-secondary px-10">Buy Stem</Button>
         </div>
 
-        <div className="flex flex-col items-end">
-          <div className="mr-auto">
+        <div className="flex flex-col items-end lg:flex-row lg:space-x-4 lg:space-y-2 lg:items-center">
+          <div className="mr-auto space-y-2 ">
             <Image
               width={300}
               alt=""
               src="https://images.unsplash.com/photo-1532187863486-abf9dbad1b69?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
           </div>
-          <Spacer y={4} />
+
           <Image
-            width={240}
+            width={200}
             alt=""
             src="https://images.unsplash.com/photo-1582560475093-ba66accbc424?q=80&w=1440&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
